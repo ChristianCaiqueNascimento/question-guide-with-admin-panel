c8b869e081acd01956ecb3ab24e8c2a6e3d99267
Christian Caique <christian.caique@hotmail.com>
2023-05-16 00:52:12 -0300

    create route articles/save - post

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('guiapress', 'root', 'C@ique9813', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 
